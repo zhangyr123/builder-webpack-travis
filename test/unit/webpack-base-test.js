352ea0998d0d12aa86ce0e587246cdb1fa72e416
zhangyr123 <zhangyr6666@gmail.com>
2020-03-13 00:20:12 +0800

    chore: update two

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,6 +5,7 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base')
 
     it('entry', () => {
+        // 覆盖率
         assert.equal(baseConfig.entry.index, 'E:/前端学习/webpack/webpack9/builder-webpack/test/smoke/template/src/index/index.js')
         assert.equal(baseConfig.entry.search, 'E:/前端学习/webpack/webpack9/builder-webpack/test/smoke/template/src/search/index.js')
     })
